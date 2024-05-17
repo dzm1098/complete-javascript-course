9431abd0e30af15cefaa4b233fea8739d78780a8
Ziming Dong <dzm1098@gmail.com>
2024-05-16 23:24:11 -0700

    One hour study May 16

---
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -0,0 +1,15 @@
+let js = 'amazing';
+if (js === 'amazing') alert('JavaScript is FUN!');
+
+console.log(40 + 8 + 23 - 10);
+console.log('Jonas');
+
+let firstName = 'Jack';
+console.log(firstName);
+
+// can change any time
+let age = 30;
+age = 31;
+
+// can't change
+const birthYear = 1991;
